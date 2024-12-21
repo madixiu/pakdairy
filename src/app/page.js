b66eaa2bb3f9ec0ac7a6f439676954b5b60b8137
madixiu <mahdi.moradi72@gmail.com>
2024-12-21 11:12:59 +0330

    +products category

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,12 @@
-import Image from "next/image";
-import SlidingProducts from "./Components/Home/SlidingProducts";
-import ProductsCategory from "./Components/Home/ProductsCategory";
+import Image from 'next/image';
+import SlidingProducts from './Components/Home/SlidingProducts';
+import ProductsCategory from './Components/Home/ProductsCategory';
 export default function Home() {
   return (
     <div>
-
       <SlidingProducts />
       <ProductsCategory />
-    <div className="grid grid-col-[20px_1fr_20px] items-center justify-items-center h-[2400px] p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-  
-
-  
+      <div className="grid grid-col-[20px_1fr_20px] items-center justify-items-center h-[2400px] p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
     </div>
-    </div>
-
   );
 }
