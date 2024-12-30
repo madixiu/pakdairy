b22610dca194fd8cd6369bd6a52539c30ea16ec0
madixiu <mahdi.moradi72@gmail.com>
2024-12-30 13:39:37 +0330

    basic ui for news

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['localhost'], // Allow images from localhost
+  },
+};
 
 export default nextConfig;
--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,7 +1,39 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { Card } from '@/components/ui/card';
+import { BentoGrid, BentoGridItem } from '@/components/ui/bento-grid';
 function News() {
-  return ( 
-    <div>here is news</div>
-   );
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/news/')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // Log the data to check its structure
+        setNews(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div className='mt-10'>
+      {news.length > 0 ? (
+        <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
+          {news.map((item, i) => (
+            <BentoGridItem
+              key={item.id}
+              title={item.title}
+              description={item.summary}
+              image={'http://localhost:8000/media/news_images/1.jpg'} // Assuming your API returns an image URL
+            />
+          ))}
+        </BentoGrid>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
 export default News;
