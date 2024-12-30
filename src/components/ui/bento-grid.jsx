e21200641267d7d130cdc046e1e19a844673f11a
madixiu <mahdi.moradi72@gmail.com>
2024-12-30 13:39:37 +0330

    basic ui for news

---
+++ src/components/ui/bento-grid.jsx
@@ -0,0 +1,38 @@
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+export const BentoGrid = ({ className, children }) => {
+  return (
+    <div
+      className={cn(
+        'grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({ title, description, image }) => {
+  return (
+    <div className="relative overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
+      <div className="h-48 bg-gray-200 relative"> {/* Fixed height for the image */}
+        {image && (
+          <Image
+            src={image}
+            alt={title}
+            layout="fill" // Fill the parent container
+            objectFit="cover" // Cover the area while maintaining aspect ratio
+            className="absolute inset-0"
+          />
+        )}
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 p-4 bg-white bg-opacity-70 transition-transform transform translate-y-0">
+        <h3 className="text-xs font-semibold">{title}</h3>
+        <span className="text-sm">{description}</span>
+      </div>
+    </div>
+  );
+};
+
+// export default BentoGridItem;
