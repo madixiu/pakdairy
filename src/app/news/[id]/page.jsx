8e5df187f43d8e2ce7f711812f4f3f60136ae719
madixiu <mahdi.moradi72@gmail.com>
2025-01-06 08:35:21 +0330

    *news detail

--- src/app/news/[id]/page.jsx
+++ src/app/news/[id]/page.jsx
@@ -1,26 +1,40 @@
 import { apiUrl } from '@/lib/api';
 import parse from 'html-react-parser';
 export default async function Page({ params }) {
-  const id = (await params).id
+  const id = (await params).id;
   // const data = await fetch('https://api.vercel.app/blog')
   // const post = await data.json()
   // Example usage in a component
 
-const fetchData = async () => {
+  const fetchData = async () => {
     const response = await fetch(apiUrl(`/api/news/${id}/`));
     const data = await response.json();
     return data;
-};
+  };
+  const fetchLastNews = async () => {
+    const response = await fetch(apiUrl(`/api/news/`));
+    const data = await response.json();
+    console.log(data.slice(0, 5));
+    
+  };
   // const content = params;
- // Fetch data and log it
- const data = await fetchData();
- console.log(data);
-  
+  // Fetch data and log it
+  const data = await fetchData();
+  const lastNews = await fetchLastNews();
+  // console.log(data);
+
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <p>{data.summary}</p>
-      <div>{parse(data.content)}</div>
+    <div className="bg-gray-500">
+      <div className="bg-white grid grid-cols-6">
+        <div className="col-span-1">
+          <span>last</span>
+        </div>
+        <div className="col-span-5 flex flex-col items-center">
+          <h1>{data.title}</h1>
+          <p>{data.summary}</p>
+          <div>{parse(data.content)}</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -4,13 +4,13 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation'; // Import useRouter
 import { Card } from '@/components/ui/card';
 import { BentoGrid, BentoGridItem } from '@/components/ui/bento-grid';
-
+import { apiUrl } from '@/lib/api';
 function News() {
   const [news, setNews] = useState([]);
   const router = useRouter(); // Initialize useRouter
 
   useEffect(() => {
-    fetch('http://192.168.20.61:8000/api/news/')
+    fetch(apiUrl(`/api/news/`))
       .then((response) => response.json())
       .then((data) => {
         console.log(data); // Log the data to check its structure
@@ -19,18 +19,10 @@ function News() {
       .catch((error) => console.error(error));
   }, []);
 
+
   // Function to handle navigation
   const handleNewsClick = (id) => {
-    console.log('====================================');
-    // console.log("clicked",item);
-    console.log('====================================');
-    console.log('Router:', router); // Check if router is null or undefined
-
     router.push(`/news/${id}`); // Navigate to the news detail page
-  //   router.push({
-  //     pathname: `/news/${item.id}`,
-  //     query: { item }, // Add your query parameters here
-  // });
   };
 
   return (
@@ -42,7 +34,7 @@ function News() {
               key={item.id}
               title={item.title}
               description={item.summary}
-              image={'http://192.168.20.61:8000/media/news_images/1.jpg'} // Assuming your API returns an image URL
+              image={item?.image ? item.image : apiUrl('/media/news_images/1.jpg') } // Assuming your API returns an image URL
               onClick={() => handleNewsClick(item.id)} // Add onClick handler
             />
           ))}
